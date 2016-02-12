8982d4b4efe5cc9f65314b2ca8a5a21becc431ae
yoshalangius <henkkooi11@gmail.com>
2016-02-12 17:01:51 +0100

    camera demo
    Als speler wil ik dat de camera mij volgt zodat ik niet buiten beeld val

---
+++ mythe/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 CamPos;
+	
+	void Start ()
+    {
+        CamPos = transform.position - target.transform.position;
+	}
+	
+
+	void LateUpdate ()
+    {
+        transform.position = target.transform.position + CamPos;
+	}
+}
---
+++ mythe/Assets/Scripts/TestCamScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCamScript : MonoBehaviour
+{
+    public float speed = 6;
+    public GameObject Box;
+	
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Box.transform.Translate(Vector3.right * speed);
+        }
+	}
+}
