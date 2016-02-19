79eb059dd6a9927715671fd4dbc53eee5aa5502d
yoshalangius <henkkooi11@gmail.com>
2016-02-19 16:07:35 +0100

    enemy behavior test
    Als speler wil ik minion behaviour zodat ik kleine vijanden heb die ik
    moet verslaan

---
+++ mythe/Assets/Scripts/EnemyScripts/SmallEnemies/EnemyBaseScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBaseScript : MonoBehaviour
+{
+    public GameObject EnimPos;
+    public float BaseSpeed = 5;
+    public bool PlayerInSight = false;
+    public float TimeLeft = 10;
+
+    //for test purposes
+    public bool Attacked = false;
+    //-----------------
+	
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        if (PlayerInSight != true)
+        {
+            EnimPos.transform.Translate(Vector3.left * BaseSpeed);
+        }
+        if (PlayerInSight == true)
+        {
+            TimeLeft -= Time.deltaTime;
+            if (TimeLeft < 0)
+            {
+                Debug.Log("i attack the enemy!");
+                Attacked = true;
+                TimeLeft = 10;
+            }
+
+            if(TimeLeft < 10 | TimeLeft == 0)
+            {
+                Attacked = false;
+            }
+        }
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("a foe!");
+        
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInSight = true;
+            
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("the foe has flead or has been vanqished");
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInSight = false;
+        }
+    }
+}
---
+++ mythe/Assets/Scripts/TestScript/TestHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHealth : MonoBehaviour
+{
+
+    public int Health = 5;
+    public EnemyBaseScript EnimProperty;
+
+	void Update ()
+    {
+
+        if (EnimProperty.Attacked == true)
+        {
+            Health = Health - 1;
+        }
+        if (Health < 0 | Health == 0)
+        {
+            Debug.Log("i have fallen");
+        }
+	}
+}
