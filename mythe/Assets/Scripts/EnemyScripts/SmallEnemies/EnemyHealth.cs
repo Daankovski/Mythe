438ecd47c50d78708594dc973af713426776d8fe
yoshalangius <henkkooi11@gmail.com>
2016-04-11 09:58:25 +0200

    enemy health
    als speler wil ik de enemy willen aanvalen

---
+++ mythe/Assets/Scripts/EnemyScripts/SmallEnemies/EnemyHealth.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int EHealth = 2;
+    public bool isELosingHP = false;
+
+    //for test purposes
+
+
+    //-----------------
+    public TestCamScript foe;
+    public PlayerAttack PlayerAtt;
+    public GameObject PlayerScript;
+
+    //and maybe more states later.
+
+    void Start()
+    {
+        PlayerScript = GameObject.FindGameObjectWithTag("Player");
+        PlayerAtt = PlayerScript.GetComponent<PlayerAttack>();
+        Debug.Log("health");
+        PlayerAtt.isPAttacking += isPAttacking;
+    }
+
+  
+
+    void isPAttacking(bool isPAttacked)
+    {
+        Debug.Log("i work");
+
+        if (isPAttacked == true)
+        {
+            Debug.Log("au");
+            isELosingHP = true;
+            isPAttacked = false;
+        }
+        Debug.Log(isPAttacked);
+        Debug.Log(EHealth);
+
+    }
+
+    void Update()
+    {
+        if (isELosingHP == true)
+        {
+           
+            EHealth = EHealth - 1;
+            isELosingHP = false;
+        }
+    }
+
+}
---
+++ mythe/Assets/Scripts/TestScript/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public bool PlayerAttacks = false;
+    public delegate void PAttack(bool PlayerAttacks);
+
+    public PAttack isPAttacking;
+
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            PlayerAttacks = true;
+            DispatchHit();
+        }
+    }
+
+    void DispatchHit()
+    {
+        Debug.Log("space");
+        if (isPAttacking != null)
+            isPAttacking(PlayerAttacks);
+    }
+}
